cb8545f97a1fe9ac1299ca389f7ba487700f99e5
davide <davide.baiocchi97@gmail.com>
2021-06-18 12:12:07 +0200

    fs

--- js/script.js
+++ js/script.js
@@ -8,102 +8,102 @@
 // Milestone 1 Partendo dalla seguente struttura dati, 
 // mostriamo in pagina tutte le icone disponibili come da layout. 
 const icons = [
-{
-    name: 'cat',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-},
-{
-    name: 'crow',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-},
-{
-    name: 'dog',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-},
-{
-    name: 'dove',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-},
-{
-    name: 'dragon',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-},
-{
-    name: 'horse',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-},
-{
-    name: 'hippo',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-},
-{
-    name: 'fish',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-},
-{
-    name: 'carrot',
-    prefix: 'fa-',
-    type: 'vegetable',
-    family: 'fas',
-},
-{
-    name: 'apple-alt',
-    prefix: 'fa-',
-    type: 'vegetable',
-    family: 'fas',
-},
-{
-    name: 'lemon',
-    prefix: 'fa-',
-    type: 'vegetable',
-    family: 'fas',
-},
-{
-    name: 'pepper-hot',
-    prefix: 'fa-',
-    type: 'vegetable',
-    family: 'fas',
-},
-{
-    name: 'user-astronaut',
-    prefix: 'fa-',
-    type: 'user',
-    family: 'fas',
-},
-{
-    name: 'user-graduate',
-    prefix: 'fa-',
-    type: 'user',
-    family: 'fas',
-},
-{
-    name: 'user-ninja',
-    prefix: 'fa-',
-    type: 'user',
-    family: 'fas',
-},
-{
-    name: 'user-secret',
-    prefix: 'fa-',
-    type: 'user',
-    family: 'fas',
-},
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
 ];
 
 // Milestone 1 - Partendo dalla seguente struttura dati , 
@@ -111,7 +111,7 @@ const icons = [
 icons.forEach((element) => {
         
 // destrutturo l'oggetto (element) per leggere le chiavi
-const {name, prefix, family} = element;
+const {name, prefix, type, family} = element;
 
 // utilizzo template literal per popolare html 
 const iconHTML = `
@@ -123,3 +123,27 @@ const iconHTML = `
 document.getElementById('icon').innerHTML += iconHTML;       
 });
     
+
+// Milestone 2 Coloriamo le icone per tipo
+const colors = [
+    'red',
+    'blue',
+    'green'
+];
+
+const colorIcon = icons.map((element) => {
+
+    return {
+        name,
+        prefix,
+        type,
+        family,
+        color : (gender == "female") ? pink : blue, 
+    }
+});
+newCats.forEach((element) => {
+    document.getElementById('item').innerHTML += `</br>
+    ${element.nome} : <i class= "fas fa-cat" style="color: ${element.colore}"></i>
+    <i class="fas fa-ribbon" style="color:${element.ribbon.color}; opacity:${element.ribbon.opacity}"></i>,  
+    `;
+});
